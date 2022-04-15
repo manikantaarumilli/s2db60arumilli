02ac7a376b52a531db1f4afa07935e9741c472c9
manikantaarumilli <98128314+manikantaarumilli@users.noreply.github.com>
2022-04-14 21:07:44 -0500

    Update jam.js

--- controllers/jam.js
+++ controllers/jam.js
@@ -44,8 +44,24 @@ exports.jam_delete = function(req, res) {
 }; 
  
 // Handle jam update form on PUT. 
-exports.jam_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: jam update PUT' + req.params.id); 
+exports.jam_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await jam.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.jam_name)  
+               toUpdate.jam_name = req.body.jam_name; 
+        if(req.body.cost) toUpdate.cost = req.body.cost; 
+        if(req.body.quantity) toUpdate.quantity = req.body.quantity; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
 }; 
 
 // List of all jams
