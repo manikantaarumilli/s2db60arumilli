ebf90be1a23be391f49f1db3f601030199a00565
manikantaarumilli <98128314+manikantaarumilli@users.noreply.github.com>
2022-04-19 21:44:07 -0500

    Update jam.js

--- controllers/jam.js
+++ controllers/jam.js
@@ -109,3 +109,17 @@ exports.jam_view_one_Page = async function(req, res) {
         res.send(`{'error': '${err}'}`); 
     } 
 }; 
+
+// Handle building the view for creating a costume. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.jam_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('jamcreate', { title: 'jam Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
