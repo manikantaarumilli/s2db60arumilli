fe141b9b84665e8d797775c091e0f189124346d5
manikantaarumilli <98128314+manikantaarumilli@users.noreply.github.com>
2022-04-19 22:39:40 -0500

    Update jam.js

--- routes/jam.js
+++ routes/jam.js
@@ -10,6 +10,7 @@ router.get('/', jam_controlers.jam_view_all_Page );
 router.get('/detail', jam_controlers.jam_view_one_Page); 
 router.get('/create', jam_controlers.jam_create_Page); 
 router.get('/update', jam_controlers.jam_update_Page); 
+router.get('/delete', jam_controlers.jam_delete_Page); 
 /*
 router.get('/', function (req, res, next) {
     res.render('jam', { title: 'search result'});
