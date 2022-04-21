f466411b3b30ff2fb88fdef12fe20b602cb01c18
manikantaarumilli <98128314+manikantaarumilli@users.noreply.github.com>
2022-04-21 15:15:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -76,7 +76,15 @@ app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
 
-
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account =require('./models/account')); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
+ 
 
 
 
