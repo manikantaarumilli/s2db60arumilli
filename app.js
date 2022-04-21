d454a8eff80aed11835274f3b99ecfecbb9b1aaa
manikantaarumilli <98128314+manikantaarumilli@users.noreply.github.com>
2022-04-21 15:08:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy;
 
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  } 
 
 const connectionString =  
 process.env.MONGO_CON 
