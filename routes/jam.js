3bf7ba5640f126e29497268fd9160f62e6ff767c
manikantaarumilli <98128314+manikantaarumilli@users.noreply.github.com>
2022-04-27 17:24:47 -0500

    Update jam.js

--- routes/jam.js
+++ routes/jam.js
@@ -1,6 +1,16 @@
 var express = require('express');
 const jam_controlers= require('../controllers/jam'); 
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on 
+or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 
 /* GET home page. */
 
@@ -9,7 +19,7 @@ var router = express.Router();
 router.get('/', jam_controlers.jam_view_all_Page ); 
 router.get('/detail', jam_controlers.jam_view_one_Page); 
 router.get('/create', jam_controlers.jam_create_Page); 
-router.get('/update', jam_controlers.jam_update_Page); 
+router.get('/update',secured, jam_controlers.jam_update_Page); 
 router.get('/delete', jam_controlers.jam_delete_Page); 
 /*
 router.get('/', function (req, res, next) {
